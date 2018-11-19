f59a92a63eec5cedf031e7e6f0ebaed0c54eaa55
zh_zhou <zh79325@163.com>
2018-11-19 09:58:46 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Jxbrowser Crack Util
+
+## Sample Code
+```$xslt
+ public static void main(String[] args) {
+        //start hack
+        JxBrowserHackUtil.hack();
+        
+        String identity = UUID.randomUUID().toString();
+        BrowserContextParams params = new BrowserContextParams("tmp/browser/" + identity);
+        BrowserContext context1 = new BrowserContext(params);
+        Browser browser = new Browser(context1);
+        BrowserView view = new BrowserView(browser);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(view,BorderLayout.CENTER);
+        frame.setSize(700,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        browser.loadURL("https://www.baidu.com/");
+
+    }
+```
+
+## Console Output
+```$xslt
+-- Product name: JxBrowser
+-- Licensed version: 6.x
+-- Licensed to: 
+-- License type: Evaluation
+-- Generation date: 2017-4-12
+-- Expiration date: 2217-6-12
+-- License info: JxBrowser Demo License
+-- Current date: 2018-11-19
+JxBrowser license valid.
+
+```
+
+
+## Support Versions
+
+
---
+++ src/main/java/potato/jx/main/SampleCode.java
@@ -0,0 +1,38 @@
+package potato.jx.main;
+
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserContext;
+import com.teamdev.jxbrowser.chromium.BrowserContextParams;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+import potato.jx.crack.JxBrowserHackUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.UUID;
+
+/**
+ * @author zh_zhou
+ * created at 2018/11/19 09:50
+ * Copyright [2018] [zh_zhou]
+ */
+public class SampleCode {
+    public static void main(String[] args) {
+        //start hack
+        JxBrowserHackUtil.hack();
+
+        String identity = UUID.randomUUID().toString();
+        BrowserContextParams params = new BrowserContextParams("temp/browser/" + identity);
+        BrowserContext context1 = new BrowserContext(params);
+        Browser browser = new Browser(context1);
+        BrowserView view = new BrowserView(browser);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(view,BorderLayout.CENTER);
+        frame.setSize(700,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        browser.loadURL("https://www.baidu.com/");
+
+    }
+}
